fa70dc849c914a5224d6ab32c5b2176ba2a92e9d
buyalsky22 <burak.dursunlar@hotmail.com>
2020-07-16 20:46:49 +0300

    send initial response which indicates authentication status from server

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use crate::config::{parse_client_config, parse_server_config, ClientConfig, Serv
 use std::env;
 use std::io;
 use std::io::{Read, Write};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
+use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpListener, TcpStream};
 use std::process::{exit, Command};
 use std::str;
 
 mod config;
 
 const NULL_RESPONSE: &[u8] = " ".as_bytes();
+const AUTHENTICATED: &[u8] = "Authenticated".as_bytes();
+const FAILED_AUTHENTICATION: &[u8] = "Failed".as_bytes();
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -26,7 +28,6 @@ fn main() {
 }
 
 fn server(config: ServerConfig) {
-    println!("server");
     let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), config.port as u16);
     let listener = TcpListener::bind(address).unwrap();
     let mut buffer = [0; 1024];
@@ -49,61 +50,78 @@ fn server(config: ServerConfig) {
 
         if *username == config.username && *password == config.password {
             println!("Authentication succeed!");
-        } else {
-            println!("Authentication failed!");
-            exit(1);
-        }
-        loop {
-            let size = stream.read(&mut buffer).unwrap();
-            let command = String::from_utf8_lossy(&buffer[..size]);
-            println!("{}", command);
-            let command: Vec<&str> = command.split(" ").collect();
-            let output = Command::new(command.get(0).unwrap().trim().clone())
-                .arg(command.get(1..).unwrap().join(" "))
-                .output();
-            match output {
-                Ok(output) => {
-                    let output = str::from_utf8(&output.stdout).unwrap().trim();
-                    println!("{:?}", output);
-                    if output.len() == 0{
-                        stream.write(NULL_RESPONSE).unwrap();
-                    }
-                    else {
-                        stream.write(output.as_bytes()).unwrap();
-                    }
+            stream.write_all(AUTHENTICATED).unwrap();
+
+            loop {
+                let size = stream.read(&mut buffer).unwrap();
+                let command = String::from_utf8_lossy(&buffer[..size]);
+                if command.len() == 0 {
+                    exit(0);
                 }
-                Err(e) => {
-                    stream
-                        .write(format!("Error while executing command ({})", e).as_bytes())
-                        .unwrap();
+                println!("{}", command);
+                let mut parts = command.trim().split_whitespace();
+                let command = parts.next().unwrap();
+                let args = parts;
+                let output = Command::new(command).args(args).output();
+                match output {
+                    Ok(output) => {
+                        let output = str::from_utf8(&output.stdout).unwrap().trim();
+                        println!("{:?}", output);
+                        if output.len() == 0 {
+                            stream.write(NULL_RESPONSE).unwrap();
+                        } else {
+                            stream.write(output.as_bytes()).unwrap();
+                        }
+                    }
+                    Err(e) => {
+                        stream
+                            .write(format!("Error while executing command ({})", e).as_bytes())
+                            .unwrap();
+                    }
                 }
             }
+        } else {
+            println!("Authentication failed!");
+            stream.write_all(FAILED_AUTHENTICATION).unwrap();
         }
     }
 }
 
 fn client(config: ClientConfig) {
-    println!("client");
     let mut buffer = [0; 1024];
     let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), config.port as u16);
     let mut stream = TcpStream::connect(address).unwrap();
-    println!("connection succeed");
+    println!("Connection established!");
     stream
         .write(format!("{} {}", config.username, config.password).as_bytes())
         .unwrap();
+
+    stream.read(&mut buffer).unwrap();
+    // TODO come up with an elegant way
+    if buffer[..AUTHENTICATED.len()].to_vec() != AUTHENTICATED.to_vec() {
+        println!("Authentication failed!");
+        exit(1);
+    }
+
     loop {
-        let mut command = String::with_capacity(1024);
+        let mut input = String::with_capacity(1024);
+        print!("> ");
+        io::stdout().flush().unwrap();
         io::stdin()
-            .read_line(&mut command)
+            .read_line(&mut input)
             .expect("Failed to read line");
 
         //remove trailing newline
-        command.truncate(command.len() - 1);
+        let command = input.trim();
 
-        println!("{:?}", command);
+        if command.ends_with("quit") {
+            stream.shutdown(Shutdown::Both).unwrap();
+            exit(0);
+        }
+
         stream.write(command.as_bytes()).unwrap();
         let size = stream.read(&mut buffer).unwrap();
         let command = String::from_utf8_lossy(&buffer[..size]);
-        println!("Response: {}", command);
+        println!("{}", command);
     }
 }
