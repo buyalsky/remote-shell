6601cab7118ccf96fd5b2820ab20397d91242ef5
burak <burak.dursunlar@hotmail.com>
2020-10-04 20:21:30 +0300

    refactor config structs

--- src/config.rs
+++ src/config.rs
@@ -14,46 +14,53 @@ pub struct ClientConfig {
     pub password: String,
 }
 
-pub fn parse_server_config(args: Vec<String>) -> ServerConfig {
-    let mut opts = Options::new();
-    opts.optopt("p", "port", "set port", "3333");
-    opts.optopt("u", "username", "set username", "user");
-    opts.optopt("P", "password", "set password", "1234");
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
-    };
-    let port = matches.opt_str("p").unwrap().parse::<usize>().unwrap();
-    let username = matches.opt_str("u").unwrap();
-    let password = matches.opt_str("P").unwrap();
-    ServerConfig {
-        port,
-        username,
-        password,
-    }
-}
-
-pub fn parse_client_config(args: Vec<String>) -> ClientConfig {
+fn init_options() -> Options{
     let mut opts = Options::new();
     opts.optopt("h", "host", "host address", "127.0.0.1");
     opts.optopt("p", "port", "set port", "3333");
     opts.optopt("u", "username", "set username", "user");
     opts.optopt("P", "password", "set password", "1234");
+    opts
+}
 
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
-    };
-    let host_address = matches.opt_str("h").unwrap();
-    let port = matches.opt_str("p").unwrap().parse::<usize>().unwrap();
-    let username = matches.opt_str("u").unwrap();
-    let password = matches.opt_str("P").unwrap();
-
-    ClientConfig {
-        host_address,
-        port,
-        username,
-        password,
+impl ServerConfig{
+    pub fn parse_server_config(args: Vec<String>) -> ServerConfig {
+        let opts = init_options();
+        let matches = match opts.parse(&args[1..]) {
+            Ok(m) => m,
+            Err(f) => panic!(f.to_string()),
+        };
+        let port = matches.opt_str("p").unwrap().parse::<usize>().unwrap();
+        let username = matches.opt_str("u").unwrap();
+        let password = matches.opt_str("P").unwrap();
+        ServerConfig {
+            port,
+            username,
+            password,
+        }
     }
 }
+
+
+impl ClientConfig{
+    pub fn parse_client_config(args: Vec<String>) -> ClientConfig {
+        let opts = init_options();
+
+        let matches = match opts.parse(&args[1..]) {
+            Ok(m) => m,
+            Err(f) => panic!(f.to_string()),
+        };
+        let host_address = matches.opt_str("h").unwrap();
+        let port = matches.opt_str("p").unwrap().parse::<usize>().unwrap();
+        let username = matches.opt_str("u").unwrap();
+        let password = matches.opt_str("P").unwrap();
+
+        ClientConfig {
+            host_address,
+            port,
+            username,
+            password,
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::process::exit;
 
 use crate::client::initialize_client;
-use crate::config::{parse_server_config, parse_client_config};
+use crate::config::{ClientConfig, ServerConfig};
 use crate::server::initialize_server;
 
 mod config;
@@ -17,8 +17,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        7 => initialize_server(parse_server_config(args)),
-        9 => initialize_client(parse_client_config(args)),
+        7 => initialize_server(ServerConfig::parse_server_config(args)),
+        9 => initialize_client(ClientConfig::parse_client_config(args)),
         _ => {
             println!("Usage for server: -p port -u username -s password");
             println!("Usage for client: -h host_address -p port -u username -s password");
